6bda738b1c5dc2446ba99565d70b48ff234aea56
bernard-ng <ngandubernard@gmail.com>
2024-05-19 22:24:46 +0200

    fix tables

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,6 +6,7 @@ class MainController < ApplicationController
   def index
     @farms_count = Farm.count_for_user(current_user.id)
     @animals_count =  Animal.count_for_user(current_user.id)
-    @diagnotics_count = Diagnostic.count_for_user(current_user.id)
+    @diagnostics_count = Diagnostic.count_for_user(current_user.id)
+    @diagnostics = Diagnostic.belonging_to_user(current_user.id).limit(5)
   end
 end
